14e3a2c06383bbc1204b0b4d41a582112f85c85c
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2020-05-10 14:29:53 -0500

    Implement display scaling

--- src/video/vita.c
+++ src/video/vita.c
@@ -34,6 +34,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include <stdarg.h>
 
@@ -41,6 +42,7 @@
 #define printf vita_debug_log
 #endif
 
+void draw_streaming(vita2d_texture *frame_texture);
 void draw_fps();
 void draw_indicators();
 
@@ -103,6 +105,83 @@ uint32_t need_drop = 0;
 uint32_t curr_fps[2] = {0, 0};
 float carry = 0;
 
+typedef struct {
+  unsigned int texture_width;
+  unsigned int texture_height;
+  float origin_x;
+  float origin_y;
+  float region_x1;
+  float region_y1;
+  float region_x2;
+  float region_y2;
+} image_scaling_settings;
+
+static image_scaling_settings image_scaling = {0};
+
+// Vita's sceVideodecInitLibrary only accept resolution that is multiple of 16 on either dimension,
+// and the smallest resolution is 64
+// Full supported resolution list can be found at:
+// https://github.com/MakiseKurisu/vita-sceVideodecInitLibrary-test/
+#define ROUND_NEAREST_16(x)                     (round(((double) (x)) / 16) * 16)
+#define VITA_DECODER_RESOLUTION_LOWER_BOUND(x)  ((x) < 64 ? 64 : (x))
+#define VITA_DECODER_RESOLUTION(x)              (VITA_DECODER_RESOLUTION_LOWER_BOUND(ROUND_NEAREST_16(x)))
+
+void update_scaling_settings(int width, int height) {
+  image_scaling.texture_width = SCREEN_WIDTH;
+  image_scaling.texture_height = SCREEN_HEIGHT;
+  image_scaling.origin_x = 0;
+  image_scaling.origin_y = 0;
+  image_scaling.region_x1 = 0;
+  image_scaling.region_y1 = 0;
+  image_scaling.region_x2 = image_scaling.texture_width;
+  image_scaling.region_y2 = image_scaling.texture_height;
+
+  double scaled_width = (double) SCREEN_HEIGHT * width / height;
+  double scaled_height = (double) SCREEN_WIDTH * height / width;
+
+  if (SCREEN_WIDTH * height == SCREEN_HEIGHT * width) {
+    // streaming resolution ratio matches Vita's screen ratio
+    // use default setting
+  } else if (SCREEN_WIDTH * height > SCREEN_HEIGHT * width) {
+    // host ratio example: 4:3, 16:10
+    // Vita ratio range: 2:16 (64 x 544) - native (960 x 544)
+    if (config.center_region_only) {
+      image_scaling.texture_height = VITA_DECODER_RESOLUTION(scaled_height);
+      image_scaling.region_y1 = VITA_DECODER_RESOLUTION((scaled_height - SCREEN_HEIGHT) / 2);
+      image_scaling.region_y2 = VITA_DECODER_RESOLUTION((scaled_height + SCREEN_HEIGHT) / 2);
+    } else {
+      image_scaling.texture_width = VITA_DECODER_RESOLUTION(scaled_width);
+      image_scaling.region_x2 = VITA_DECODER_RESOLUTION(scaled_width);
+      image_scaling.origin_x = round((double) (SCREEN_WIDTH - image_scaling.texture_width) / 2);
+    }
+  } else {
+    // host ratio example: 16:9, 21:9, 32:9
+    // Vita ratio range: native (960 x 544) - 15:1 (960 x 64)
+    if (config.center_region_only) {
+      image_scaling.texture_width = VITA_DECODER_RESOLUTION(scaled_width);
+      image_scaling.region_x1 = VITA_DECODER_RESOLUTION((scaled_width - SCREEN_WIDTH) / 2);
+      image_scaling.region_x2 = VITA_DECODER_RESOLUTION((scaled_width + SCREEN_WIDTH) / 2);
+    } else {
+      image_scaling.texture_height = VITA_DECODER_RESOLUTION(scaled_height);
+      image_scaling.region_y2 = VITA_DECODER_RESOLUTION(scaled_height);
+      image_scaling.origin_y = round((double) (SCREEN_HEIGHT - image_scaling.texture_height) / 2);
+    }
+  }
+
+  printf("update_scaling_settings: width = %u\n", width);
+  printf("update_scaling_settings: height = %u\n", height);
+  printf("update_scaling_settings: scaled_width = %f\n", scaled_width);
+  printf("update_scaling_settings: scaled_height = %f\n", scaled_height);
+  printf("update_scaling_settings: image_scaling.texture_width = %u\n", image_scaling.texture_width);
+  printf("update_scaling_settings: image_scaling.texture_height = %u\n", image_scaling.texture_height);
+  printf("update_scaling_settings: image_scaling.origin_x = %f\n", image_scaling.origin_x);
+  printf("update_scaling_settings: image_scaling.origin_y = %f\n", image_scaling.origin_y);
+  printf("update_scaling_settings: image_scaling.region_x1 = %f\n", image_scaling.region_x1);
+  printf("update_scaling_settings: image_scaling.region_y1 = %f\n", image_scaling.region_y1);
+  printf("update_scaling_settings: image_scaling.region_x2 = %f\n", image_scaling.region_x2);
+  printf("update_scaling_settings: image_scaling.region_y2 = %f\n", image_scaling.region_y2);
+}
+
 static int vita_pacer_thread_main(SceSize args, void *argp) {
   // 1s
   int wait = 1000000;
@@ -156,19 +235,21 @@ static int vita_pacer_thread_main(SceSize args, void *argp) {
 }
 
 static void vita_cleanup() {
-  int ret;
   if (video_status == INIT_FRAME_PACER_THREAD) {
     active_pacer_thread = false;
     // wait 10sec
     SceUInt timeout = 10000000;
+    int ret;
     sceKernelWaitThreadEnd(pacer_thread, &ret, &timeout);
     sceKernelDeleteThread(pacer_thread);
     video_status--;
   }
+
   if (video_status == INIT_AVC_DEC) {
     sceAvcdecDeleteDecoder(decoder);
     video_status--;
   }
+
   if (video_status == INIT_DECODER_MEMBLOCK) {
     if (decoderblock >= 0) {
       sceKernelFreeMemBlock(decoderblock);
@@ -184,6 +265,7 @@ static void vita_cleanup() {
     }
     video_status--;
   }
+
   if (video_status == INIT_AVC_LIB) {
     sceVideodecTermLibrary(SCE_VIDEODEC_TYPE_HW_AVCDEC);
 
@@ -195,6 +277,11 @@ static void vita_cleanup() {
   }
 
   if (video_status == INIT_FRAMEBUFFER) {
+    if (frame_texture != NULL) {
+      vita2d_free_texture(frame_texture);
+      frame_texture = NULL;
+    }
+
     if (decoder_buffer != NULL) {
       free(decoder_buffer);
       decoder_buffer = NULL;
@@ -220,15 +307,22 @@ static int vita_setup(int videoFormat, int width, int height, int redrawRate, vo
 
   if (video_status == INIT_GS) {
     // INIT_FRAMEBUFFER
+    update_scaling_settings(width, height);
+
     decoder_buffer = malloc(DECODER_BUFFER_SIZE);
     if (decoder_buffer == NULL) {
       printf("not enough memory\n");
       ret = VITA_VIDEO_ERROR_NO_MEM;
       goto cleanup;
     }
-    if (!frame_texture) {
-      frame_texture = vita2d_create_empty_texture_format(SCREEN_WIDTH, SCREEN_HEIGHT, SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_ABGR);
+
+    frame_texture = vita2d_create_empty_texture_format(image_scaling.texture_width, image_scaling.texture_height, SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_ABGR);
+    if (frame_texture == NULL) {
+      printf("not enough memory\n");
+      ret = VITA_VIDEO_ERROR_NO_MEM;
+      goto cleanup;
     }
+
     video_status++;
   }
 
@@ -243,14 +337,8 @@ static int vita_setup(int videoFormat, int width, int height, int redrawRate, vo
       }
     }
     init->size = sizeof(SceVideodecQueryInitInfoHwAvcdec);
-    init->horizontal = width;
-    init->vertical = height;
-    // XXX: specialized setup for 960x540
-    // when we pass just 960x540 instead 960x544, sceVideodecInitLibrary
-    // would be failed
-    if (width == 960 && height == 540) {
-      init->vertical = 544;
-    }
+    init->horizontal = VITA_DECODER_RESOLUTION(width);
+    init->vertical = VITA_DECODER_RESOLUTION(height);
     init->numOfRefFrames = 5;
     init->numOfStreams = 1;
 
@@ -359,9 +447,9 @@ static int vita_submit_decode_unit(PDECODE_UNIT decodeUnit) {
 
   picture.size = sizeof(picture);
   picture.frame.pixelType = 0;
-  picture.frame.framePitch = LINE_SIZE;
-  picture.frame.frameWidth = SCREEN_WIDTH;
-  picture.frame.frameHeight = SCREEN_HEIGHT;
+  picture.frame.framePitch = image_scaling.texture_width;
+  picture.frame.frameWidth = image_scaling.texture_width;
+  picture.frame.frameHeight = image_scaling.texture_height;
   picture.frame.pPicture[0] = vita2d_texture_get_datap(frame_texture);
 
   if (decodeUnit->fullLength >= DECODER_BUFFER_SIZE) {
@@ -407,19 +495,17 @@ static int vita_submit_decode_unit(PDECODE_UNIT decodeUnit) {
       need_drop--;
     } else {
       vita2d_start_drawing();
-      void *tex_buf = vita2d_texture_get_datap(frame_texture);
-      vita2d_draw_texture(frame_texture, 0, 0);
+      
+      draw_streaming(frame_texture);
       draw_fps();
       draw_indicators();
+      
       vita2d_end_drawing();
+
       vita2d_wait_rendering_done();
       vita2d_swap_buffers();
 
-      if (ret < 0) {
-        printf("Failed to sceDisplaySetFrameBuf: 0x%x\n", ret);
-      } else {
-        frame_count++;
-      }
+      frame_count++;
     }
   }
 
@@ -427,6 +513,18 @@ static int vita_submit_decode_unit(PDECODE_UNIT decodeUnit) {
   //   return DR_NEED_IDR;
 
   return DR_OK;
+}
+
+void draw_streaming(vita2d_texture *frame_texture) {
+  // ui is still rendering in the background, clear the screen first
+  vita2d_clear_screen();
+  vita2d_draw_texture_part(frame_texture,
+                           image_scaling.origin_x,
+                           image_scaling.origin_y,
+                           image_scaling.region_x1,
+                           image_scaling.region_y1,
+                           image_scaling.region_x2,
+                           image_scaling.region_y2);
 }
 
 void draw_fps() {
